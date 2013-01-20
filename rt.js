116de6436f9d199e453f473dd1728166a5690576
Drew Stricklad <andrew.flagship@gmail.com>
2013-01-19 23:18:51 -0700

    presentation upload and basic display

--- rt.js
+++ rt.js
@@ -4,7 +4,9 @@ var connect = require('connect'),
 	quip = require('quip'),
 	usergrid = require('usergrid'),
 	swig = require('swig'),
-	keys = require(__dirname + '/keys');
+	keys = require(__dirname + '/keys'),
+	PATH = require('path'),
+	fs   = require('fs');
 
 swig.init({
 	cache: false,
@@ -27,7 +29,32 @@ function view(file, ctx){
 	return tpl.render(ctx);
 }
 
+function getDeckBySessionId(id, cb){
+	client.request({
+		method: 'GET',
+		endpoint: 'deckSessions',
+		uuid: id
+	},function(err,data){
+		if(err){ cb('not a real deck session'); return; }
+		var deckId = data.entities[0].relatedDeck;
+		client.request({
+			method: 'GET',
+			endpoint: 'decks',
+			uuid: deckId
+		},function(error,data){
+			if(error){ cb('not a real deck'); return; }
+			var deck = data.entities[0];
+			var deckFile = deck.fileName;
+			fs.readFile(__dirname+'/ups/'+deckFile, 'utf8', function(error2,data){
+				if(error2){ cb('unable to read deck file'); return; }
+				cb(false, deck, data);
+			});
+		});
+	});
+}
+
 var app = connect(quip())
+	.use(connect.multipart({ uploadDir: __dirname + '/ups' }))
 	.use(connect.static(__dirname + '/public'))
 	.use(connect.cookieParser('Archimedes'))
 	.use(connect.session({ secret: 'Merlin had an Owl, hoot hoot!'}))
@@ -112,8 +139,8 @@ var app = connect(quip())
 								console.log('could not get logged in user');
 							} else {
 								//you can then get info from the user entity object:
-								req.session.user = user;
-								res.redirect('/');
+								req.session.user = data.entities[0];
+								res.redirect('/presentations');
 							}
 						});
 					});
@@ -126,41 +153,75 @@ var app = connect(quip())
 		},
 		'/presentations': {
 			'GET': function(req,res,next){
-				res.error('Stop doing that');
+				client.createCollection({
+					type: 'decks',
+					ql: 'where fromUser = \'' + req.session.user.uuid + '\' order by index'
+				}, function(err, decks){
+					if(err){
+						res.error('unable to get presentations');
+						return;
+					}
+					req.ctx.presentations = new Array();
+					while(decks.hasNextEntity()) {
+						var deck = decks.getNextEntity()._data;
+						req.ctx.presentations.push(deck);
+					}
+					res.ok(view('my-profile.html', req.ctx));
+				});
 			},
 			'POST': function(req,res,next){
-				res.error('Stop doing that');
-			},
-			':/id':{
-				'GET': function(req,res,next,id){},
-				'PUT': function(req,res,next,id){},
-				'DELETE': function(req,res,next,id){},
+				var tup = req.files.file;
+				var upa = tup.path.split(PATH.sep),
+					ufn = upa[upa.length - 1];
+
+				var deck = {
+					type: 'decks',
+					name: tup.name,
+					fromUser: req.session.user.uuid,
+					fileName: ufn
+				};
+				// fire and forget, create deck
+				client.createEntity(deck, function(err,data){});
+				res.redirect('/presentations');
 			}
 		},
-		'/new-rt/:presID': function(req,res,next,presID){
-			res.error('Stop doing that');
+		'/set-the-table/:id': function(req,res,next,id){
+			client.request({
+				method: 'GET',
+				endpoint: 'decks',
+				uuid: id
+			}, function(err, data){
+				if(err){
+					res.error('failed to load presentation ' + id)
+				}
+				// there should be exactly 1 entities
+				var deck = data.entities[0];
+				req.session.currentDeck = deck;
+				var deckSession = {
+					type: 'deckSessions',
+					relatedDeck: deck.uuid
+				};
+				// fire and forget, create deck
+				client.createEntity(deckSession, function(err,data){
+					if(err){ res.error('Could not set the table'); return; }
+					req.session.currentDeckRoom = data._data.uuid;
+					res.redirect('/big-screen/'+data._data.uuid);
+				});
+			});
 		},
-		'/present-rt/:presID': function(req,res,next,presID){
-			res.error('Stop doing that');
-				
+		'/present/:id': function(req,res,next,id){},
+		'/big-screen/:id': function(req,res,next,id){
+			getDeckBySessionId(id, function(err,deck,deckFile){
+				req.ctx.deckContents = deckFile;
+				req.ctx.deck = deck;
+				res.ok(view('presentation.html',req.ctx))
+			});
 		},
-		'/watch/:presID': function(req,res,next,presID){
-			res.error('Stop doing that');
-				
-		},
-		'/round-tables': {
-			'GET': function(){},
-			'POST': function(req,res,next){},
-			':/id':{
-				'GET': function(req,res,next,id){},
-				'PUT': function(req,res,next,id){},
-				'DELETE': function(req,res,next,id){},
-			}
-		},
+		'/view/:id': function(req,res,next,id){},
 		'.+': function(req,res,next){
 			res.ok(view('index.html', req.ctx));
 		}
-	}))
+	}));
 
 
 var server = app.listen(4295);
